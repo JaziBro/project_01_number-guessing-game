dbf0c109211b73931a6ceaacfbec026eb1d5ecc8
jazilhashmi <138767638+JaziBro@users.noreply.github.com>
2023-09-03 20:54:10 +0500

    message

---
+++ main.ts
@@ -0,0 +1,13 @@
+import inquirer from "inquirer"
+
+let computerGuess = Math.round(Math.random()*10)
+let answers = await inquirer.prompt([
+    {
+        name: "userGuess",
+        type: "number",
+        message: "Enter your guess between 1 to 10"
+    }
+])
+if (computerGuess === answers.userGuess){
+    console.log ("Congratulations! You Won")
+} else console.log (`The number was ${computerGuess}, Sorry you lost the game`)
